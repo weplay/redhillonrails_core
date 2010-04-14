95122eda71af8b56f3eed9c7637ce635596da3c0
lukemelia <luke@lukemelia.com>
2010-04-14 12:28:35 -0400

    Only load plugin code when migrations are being run.

--- lib/redhillonrails_core.rb
+++ lib/redhillonrails_core.rb
@@ -1,19 +1,44 @@
-ActiveRecord::Base.send(:include, RedHillConsulting::Core::ActiveRecord::Base)
-ActiveRecord::Schema.send(:include, RedHillConsulting::Core::ActiveRecord::Schema)
-ActiveRecord::SchemaDumper.send(:include, RedHillConsulting::Core::ActiveRecord::SchemaDumper)
-ActiveRecord::ConnectionAdapters::IndexDefinition.send(:include, RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::IndexDefinition)
-ActiveRecord::ConnectionAdapters::TableDefinition.send(:include, RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::TableDefinition)
-ActiveRecord::ConnectionAdapters::Column.send(:include, RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::Column)
-ActiveRecord::ConnectionAdapters::AbstractAdapter.send(:include, RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::AbstractAdapter)
-ActiveRecord::ConnectionAdapters::SchemaStatements.send(:include, RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::SchemaStatements)
+require 'active_record/migration'
 
-if defined?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter) then
-  ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.send(:include, RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::PostgresqlAdapter)
+module ActiveRecord
+  class Migrator
+
+    def self.new(*args)
+      instance = super
+      ensure_redhill_on_rails_core_loaded
+      instance
+    end
+
+    def self.ensure_redhill_on_rails_core_loaded
+      return if @__redhill_on_rails_core_loaded == true
+      
+      ActiveRecord::Base.send(:include, RedHillConsulting::Core::ActiveRecord::Base)
+      ActiveRecord::Schema.send(:include, RedHillConsulting::Core::ActiveRecord::Schema)
+      ActiveRecord::SchemaDumper.send(:include, RedHillConsulting::Core::ActiveRecord::SchemaDumper)
+      ActiveRecord::ConnectionAdapters::IndexDefinition.send(:include, RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::IndexDefinition)
+      ActiveRecord::ConnectionAdapters::TableDefinition.send(:include, RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::TableDefinition)
+      ActiveRecord::ConnectionAdapters::Column.send(:include, RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::Column)
+      ActiveRecord::ConnectionAdapters::AbstractAdapter.send(:include, RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::AbstractAdapter)
+      ActiveRecord::ConnectionAdapters::SchemaStatements.send(:include, RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::SchemaStatements)
+
+      if defined?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter) then
+        ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.send(:include, RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::PostgresqlAdapter)
+      end
+      if defined?(ActiveRecord::ConnectionAdapters::MysqlAdapter) then
+        ActiveRecord::ConnectionAdapters::MysqlColumn.send(:include, RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::MysqlColumn)
+        ActiveRecord::ConnectionAdapters::MysqlAdapter.send(:include, RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::MysqlAdapter)
+      end
+      if defined?(ActiveRecord::ConnectionAdapters::SQLite3Adapter) then
+        ActiveRecord::ConnectionAdapters::SQLite3Adapter.send(:include, RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::Sqlite3Adapter)
+      end
+      
+      @__redhill_on_rails_core_loaded == true
+    end
+  end
+    
 end
-if defined?(ActiveRecord::ConnectionAdapters::MysqlAdapter) then
-  ActiveRecord::ConnectionAdapters::MysqlColumn.send(:include, RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::MysqlColumn)
-  ActiveRecord::ConnectionAdapters::MysqlAdapter.send(:include, RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::MysqlAdapter)
-end
-if defined?(ActiveRecord::ConnectionAdapters::SQLite3Adapter) then
-  ActiveRecord::ConnectionAdapters::SQLite3Adapter.send(:include, RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::Sqlite3Adapter)
-end
+
+
+
+
+
